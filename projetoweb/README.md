adebc42362d2534855ee72f7d0cdcaf289fad220
RONDINELI DE LIMA CRUZ <rondineli.cruz@univille.br>
2022-09-12 15:39:45 -0300

    Aplicação Monolítica WEB desenvolvida em NodeJS

---
+++ projetoweb/README.md
@@ -0,0 +1,23 @@
+# Projetho aplicação WEB utilizando NODE JS
+Objetivo: Aplicação monolítica WEB desenvolvida em NodeJS
+
+## Tecnologias
+- NodeJS - runtime JavaScript
+- Express - framework de desenvolvimento WEB
+
+## Como foi criado
+- Criar uma pasta com o nome do projeto
+- Executamos o comando: npm init
+- Instalou a dependência do Express: npm i express --save
+- Instalou dependencias do express-handlebars
+  npm i express-handlebars --save
+  npm i bootstrap --save
+  npm i body-parser --save
+  npm i jquery --save
+
+
+## Como executar
+- Abrir um terminal
+- Entrar na pasta do projeto: cd projetoweb
+- Executa o NodeJS: node index.js
+- Abrir o navegador no endereço: http://localhost:3000/
---
+++ projetoweb/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const { engine } = require('express-handlebars');
+const path = require('path');
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', engine());
+
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+
+app.get("/", function(req,res){
+  //  res.send(`<h1>EU NÃO ACREDITO ${req.query['nome']} ${req.query['sobrenome']}</h1>`);
+ res.render('index'); 
+})
+
+app.listen(3000, () =>{ // CALLBACK
+    console.log("Servidor online");
+    console.log("http://localhost:3000/");
+});
